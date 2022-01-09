85423696c338753b00952c2f5ff68ea1bef3f2b8
kai <kai@kaiconsulting.co>
2022-01-08 22:27:46 -0700

    created package.json file, installed inquirer, created variables in index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+inquirer
+    .prompt([
+        {
+
+        },
+        {
+
+        },
+        {
+
+        },
+        {
+
+        }
+    ])
